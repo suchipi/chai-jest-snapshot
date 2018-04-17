6e5b8cd523da3cc98958e8494d35616a7143aa07
Andres Täht <andres.taht@zeroturnaround.com>
2018-04-17 14:56:44 +0300

    Fix all other tests.

--- examples/3_manualMode/spec.js
+++ examples/3_manualMode/spec.js
@@ -1,17 +1,24 @@
 const expect = require("chai").expect;
+const chaiJestSnapshot = require("../../dist/");
 
 describe("using chaiJestSnapshot in manual mode", function() {
+  const snapshotFilename = __filename + ".snap";
+
+  beforeEach(function() {
+    chaiJestSnapshot.createState(snapshotFilename);
+  });
+
   it("takes the snapshot filename and name as arguments", function() {
-    expect({ foo: "bar" }).to.matchSnapshot(__filename + ".snap", "snapshot 1");
+    expect({ foo: "bar" }).to.matchSnapshot(snapshotFilename, "snapshot 1");
   });
 
   it("uses an optional third argument to update a snapshot", function() {
-    expect({ foo: "not bar" }).to.matchSnapshot(__filename + ".snap", "snapshot 2", true);
+    expect({ foo: "not bar" }).to.matchSnapshot(snapshotFilename, "snapshot 2", true);
   });
 
   it("can use an environment variable to update the snapshot instead", function() {
     process.env.CHAI_JEST_SNAPSHOT_UPDATE_ALL = "true";
-    expect({ foo: "something else" }).to.matchSnapshot(__filename + ".snap", "snapshot 3");
+    expect({ foo: "something else" }).to.matchSnapshot(snapshotFilename, "snapshot 3");
     delete process.env.CHAI_JEST_SNAPSHOT_UPDATE_ALL;
   });
 });
